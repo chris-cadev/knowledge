f40a1e22976b852180eabdce2b172bcedafa2ab2
Christian Camacho <chris.ca.dev@gmail.com>
2022-07-12 21:38:37 -0700

    modify a script to copy all links beetween notes

--- copy_linked_notes.sh
+++ copy_linked_notes.sh
@@ -2,21 +2,36 @@
 
 source "$(dirname "$0")/obsidian.sh"
 
+get_no_copied_files() {
+    local root="$1"
+    local resources="$2"
+    IFS=$'\n'
+    for link_name in `get_links_not_in_directory $root | sort | uniq`; do
+        for file in `find $resources -type f -name "*$link_name.md"`; do
+            local file_in_root=`echo "$root/$(echo $file | sed "s/$resources\///")"`
+            if [[ ! -f $file_in_root ]]; then
+                echo "$file;$file_in_root"
+            fi
+        done
+    done
+    unset IFS
+}
+
+
 root=${1:-'content'}
 resources=${2:-'files'}
+files_left_to_copy=`get_no_copied_files "$root" "$resources" | wc -l`
 
 IFS=$'\n'
-for link_name in `get_links_not_in_directory $root | sort | uniq`; do
-    for file in `find $resources -type f -name "*$link_name.md"`; do
-        target_directory=`dirname $file | sed "s/$resources\///"`
-        # TODO: extract this statement to a function
-        if [[ "$target_directory" == *"Zettelkasten"* ]]; then
-            continue
-        fi
-        file_name=`basename $file`
-        target_file="$root/$target_directory/$file_name"
-        mkdir -p "`dirname $target_file`"
-        cp "$file" "$target_file"
+while [ $files_left_to_copy != "0" ];
+do
+    for resource_root in `get_no_copied_files "$root" "$resources" | grep -v "Zettelkasten"`; do
+        resource_file_path=`echo $resource_root | cut -d';' -f1`
+        root_file_path=`echo $resource_root | cut -d';' -f2`
+        target_directory="`dirname "$root_file_path"`"
+        mkdir -p "$target_directory"
+        cp "$resource_file_path" "$root_file_path"
     done
+    files_left_to_copy=`get_no_copied_files "$root" "$resources" | grep -v "Zettelkasten" | wc -l`
 done
 unset IFS
