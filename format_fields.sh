27ae8bf0be46f0535e5307ec9e17699ac4de4775
Christian Camacho <chris.ca.dev@gmail.com>
2022-07-14 15:00:32 -0700

    modify fields format to add them to the end

--- format_fields.sh
+++ format_fields.sh
@@ -16,19 +16,19 @@ get_fields() {
     cat "$file" | grep -oP ".+::.+"
 }
 
-fields_to_table() {
+format_fields() {
     local file=$1
+    echo "## References"
     echo ""
-    echo "<table>"
     for field in `get_fields "$file"`; do
         local key="`echo $field | awk -F '::' '{print $1}' | xargs`"
         local value="`echo $field | awk -F '::' '{print $2}' | xargs`"
-        echo "<tr>"
-        echo "<td> ${key##*( )} </td>"
-        echo "<td> `value_to_link $value` </td>"
-        echo "</tr>"
+        if [[ $key == 'status' || $key == 'type' ]]; then
+            continue
+        fi
+        echo "### ${key##*( )}"
+        echo "> $value"
     done
-    echo "</table>"
     echo ""
 }
 
@@ -45,8 +45,8 @@ for file in `find $root -type f -name "*.md"`; do
     if [[ `get_fields "$file" | wc -l` -lt "1" ]]; then
         continue
     fi
-    new_file_content=`fields_to_table $file ; cat "$file"`
-    cat <<< $new_file_content > "$file"
+    new_file_content=`format_fields $file`
+    cat <<< $new_file_content >> "$file"
     remove_fields_from_file "$file"
 done
 unset IFS
